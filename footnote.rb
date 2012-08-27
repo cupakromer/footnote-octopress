b03d39a14e186b2f71d6e485db50c7a4abb3ba73
Aaron Kromer <aaron.kromer@gmail.com>
2012-08-26 20:05:52 -0400

    Change tabs to two spaces.

--- footnote.rb
+++ footnote.rb
@@ -18,61 +18,61 @@
 # This text needs to point to a footnote<sup id="fnref:1"><a href="#fn:1" rel="footnote">1</a></sup> because it's good<sup id="fnref:2"><a href="#fn:2" rel="footnote">2</a></sup>.
 #
 # <div class="footnotes">
-# 	<ol>
-# 		<li id="fn:1">A footnote is this text at the end of you page that explains something.<a href="#fnref:1" rev="footnote">↩</a></li>
-# 		<li id="fn:2">It's good because I like it.<a href="#fnref:2" rev="footnote">↩</a></li>
-# 	</ol>
+#   <ol>
+#     <li id="fn:1">A footnote is this text at the end of you page that explains something.<a href="#fnref:1" rev="footnote">↩</a></li>
+#     <li id="fn:2">It's good because I like it.<a href="#fnref:2" rev="footnote">↩</a></li>
+#   </ol>
 # </div>
 #
 
 
 module Footnote
 
-	class ReferenceTag < Liquid::Tag
-		def initialize(tag_name, ref, tokens)
-			super
-			@reference = ref.strip unless ref.nil?
-		end
+  class ReferenceTag < Liquid::Tag
+    def initialize(tag_name, ref, tokens)
+      super
+      @reference = ref.strip unless ref.nil?
+    end
 
-		def render(context)
-			unless @reference.nil?
-				"<sup id='fnref:#{@reference}'><a href='#fn:#{@reference}' rel='footnote'>#{@reference}</a></sup>"
-			end
-		end
-	end
+    def render(context)
+      unless @reference.nil?
+        "<sup id='fnref:#{@reference}'><a href='#fn:#{@reference}' rel='footnote'>#{@reference}</a></sup>"
+      end
+    end
+  end
 
-	class FootnotesBlock < Liquid::Block
-		def initialize(tag_name, markup, tokens)
-			@footnotes = []
-			@current_reference = 1
-			super
-		end
+  class FootnotesBlock < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      @footnotes = []
+      @current_reference = 1
+      super
+    end
 
-		def render(context)
-			<<-end_html
+    def render(context)
+      <<-end_html
 <div class="footnotes">
-	<ol>
-		#{@footnotes.join}
-	</ol>
+  <ol>
+    #{@footnotes.join}
+  </ol>
 </div>
-			end_html
-		end
+      end_html
+    end
 
-		def unknown_tag(name, params, tokens)
-			if name == 'fn'
-				handle_fn_tag(params)
-			else
-				super
-			end
-		end
+    def unknown_tag(name, params, tokens)
+      if name == 'fn'
+        handle_fn_tag(params)
+      else
+        super
+      end
+    end
 
-		private
-		def handle_fn_tag(params)
-			ref_text = params
-			@footnotes << "<li id='fn:#{@current_reference}'>#{ref_text}<a href='#fnref:#{@current_reference}' rev='footnote'>↩</a></li>"
-			@current_reference += 1
-		end
-	end
+    private
+    def handle_fn_tag(params)
+      ref_text = params
+      @footnotes << "<li id='fn:#{@current_reference}'>#{ref_text}<a href='#fnref:#{@current_reference}' rev='footnote'>↩</a></li>"
+      @current_reference += 1
+    end
+  end
 end
 
 Liquid::Template.register_tag('footnote_ref', Footnote::ReferenceTag)
